03f1be85aa33b43f8ab16ceb1a99eee6e65d663c
Yaroslav <hodosov.y@mail.ru>
2022-01-17 22:48:01 +0300

    feat: fetch json

--- lib/pages/homa_page.dart
+++ lib/pages/homa_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fradio/model/radio.dart';
 import 'package:fradio/utils/ai_util.dart';
 import 'package:velocity_x/velocity_x.dart';
 
@@ -10,9 +12,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late List<MyRadio> radios;
+
   @override
   void initState() {
     super.initState();
+    fetchRadios();
+  }
+
+  fetchRadios() async {
+    final radioJson = await rootBundle.loadString('assets/radio.json');
+    radios = MyRadioList.fromJson(radioJson).radios;
   }
 
   @override
